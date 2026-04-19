d6ed52e6e8f66fecdc50cdfadd3a3e5b67ad33f1
tahaELKHAILI <tahaelkhaili@gmail.com>
2026-04-19 18:56:27 +0100

    REST controller v2

--- TP2/src/main/java/ma/enset/tp2/Repository/ProductRepository.java
+++ TP2/src/main/java/ma/enset/tp2/Repository/ProductRepository.java
@@ -3,6 +3,9 @@ package ma.enset.tp2.Repository;
 import ma.enset.tp2.Entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    List<Product> findByNameContaining(String name);
 }
--- TP2/src/main/java/ma/enset/tp2/Web/ProductRESTController.java
+++ TP2/src/main/java/ma/enset/tp2/Web/ProductRESTController.java
@@ -3,9 +3,8 @@ package ma.enset.tp2.Web;
 import ma.enset.tp2.Entity.Product;
 import ma.enset.tp2.Repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import java.util.List;
 
@@ -26,5 +25,21 @@ public class ProductRESTController {
         return productRepository.findById(id).orElseThrow(()-> new RuntimeException("ID not found"));
     }
 
+    //Get products with name containingg keyword
+    @GetMapping("/find/products")
+    public List<Product> getProductByKyWord(@RequestParam(name = "kw", defaultValue = "") String kw){
+        return productRepository.findByNameContaining(kw);
+    }
 
+    //Add product to bd
+    @PostMapping("/products")
+    public Product addProduct(@RequestBody Product product){
+        return productRepository.save(product);
+    }
+
+    //Delete product by ID
+    @DeleteExchange("/products/{id}")
+    public void deleteProductByID(@PathVariable Long id){
+        productRepository.deleteById(id);
+    }
 }
