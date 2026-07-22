570797af80f38e1aabbbb26a76152c5b07021820
tahaELKHAILI <tahaelkhaili@gmail.com>
2026-07-22 22:52:15 +0100

    account history

--- Projet Spring Angular JWT Spring Security/frontend/BankApp/src/app/components/operations-component/operations-component.ts
+++ Projet Spring Angular JWT Spring Security/frontend/BankApp/src/app/components/operations-component/operations-component.ts
@@ -3,29 +3,52 @@ import { AccountService } from '../../services/account-service';
 import { ActivatedRoute } from '@angular/router';
 import { OperationModel } from '../../models/operation-model';
 import { Observable } from 'rxjs';
-import { AsyncPipe, DatePipe, DecimalPipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-operations',
-  imports: [AsyncPipe, DecimalPipe, DatePipe],
+  imports: [DecimalPipe, DatePipe, ReactiveFormsModule],
   templateUrl: './operations-component.html',
   styleUrl: './operations-component.css',
 })
 export class OperationsComponent implements OnInit {
   accountID!: string;
-  operationsObservable!: Observable<OperationModel>;
+  accountHistory!:OperationModel;
+  protected searchHistoryFormGroup!: FormGroup;
+
   constructor(
     private accountService: AccountService,
     private route: ActivatedRoute,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
+
     this.accountID = this.route.snapshot.params['id'];
-    this.handleGetHistory(this.accountID);
+    if(this.accountID != null){
+      this.searchHistoryFormGroup = this.formBuilder.group({
+        searchAccount: this.formBuilder.control(this.accountID, [Validators.required]),
+      });
+      this.handleGetHistory();
+    }
+    else{
+      this.searchHistoryFormGroup = this.formBuilder.group({
+        searchAccount: this.formBuilder.control(null, [Validators.required]),
+      });
+    }
   }
 
-  handleGetHistory(accountID: String) {
-    this.operationsObservable = this.accountService.accountHistory(this.accountID, 0, 5);
-    console.log(this.operationsObservable);
+  handleGetHistory(){
+    this.accountID = this.searchHistoryFormGroup.value["searchAccount"];
+    this.accountService.accountHistory(this.accountID, 0, 5).subscribe({
+      next: resp => {
+        this.accountHistory = resp;
+      },
+      error: err => {
+        alert("Account does not exist.")
+        this.searchHistoryFormGroup.reset()
+      }
+    });
   }
 }
